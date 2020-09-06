a90be397dfd1b3673d98e77d8d9dde71d9b46954
LauraNancyAnteloDorado <a2019112167@estudiantes.upsa.edu.bo>
2020-09-06 16:45:15 -0400

    Proyecto inicial con Accounts

---
+++ AppiSeccureStudent/AppiSeccureStudent/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AppiSeccureStudent.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AppiSeccureStudent/AppiSeccureStudent/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppiSeccureStudent.WebApiApplication" Language="C#" %>
