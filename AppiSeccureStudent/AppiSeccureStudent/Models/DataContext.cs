18e3a489b4ef4ee5b45a8eb1ce4ec30ec2ef0cc9
LauraNancyAnteloDorado <a2019112167@estudiantes.upsa.edu.bo>
2020-09-18 12:34:10 -0400

    con controlador de Students mas autorizacion

---
+++ AppiSeccureStudent/AppiSeccureStudent/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+namespace AppiSeccureStudent.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<AppiSeccureStudent.Models.Student> Students { get; set; }
+    }
+}
